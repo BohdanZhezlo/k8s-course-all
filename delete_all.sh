a496bc0e3cf73c53b6f6c5c7f24008ab97fb6ec5
BohdanZhezlo <bohdan.zhezlo@yahoo.com>
2020-05-04 14:22:14 +0300

    homework

--- delete_all.sh
+++ delete_all.sh
@@ -2,7 +2,5 @@ kubectl delete -f ./k8s/configs.yaml
 kubectl delete -f ./k8s/secrets.yaml
 kubectl delete -f ./k8s/mysql.yaml
 kubectl delete -f ./k8s/user-manager-app.yaml
-#kubectl delete -f ./k8s/user-manager-app-v1.yaml
-#kubectl delete -f ./k8s/user-manager-app-v2.yaml
 kubectl delete -f ./k8s/load-balancer-service.yaml
 kubectl delete -f ./k8s/ingress.yaml
